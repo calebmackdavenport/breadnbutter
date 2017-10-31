2deefbf1013175a70f9bf45e2b8764c39f83f540
Stinson Lyon <Stinzo@Stinson-Lyons-MacBook-Pro.local>
2017-10-31 12:58:22 -0500

    Setting up app for heroku deployment

--- server/index.js
+++ server/index.js
@@ -41,6 +41,6 @@ app.get('/api/ingredients', function (req, res) {
 app.get('/', function (req, res) {
     res.send(fs.readFileSync('index.html', "utf8"));
 });
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('Listening on port 3000.');
 });
