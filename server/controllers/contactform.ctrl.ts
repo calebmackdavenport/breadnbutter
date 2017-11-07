3740cf98c775f2be62b192dfd9199f1356b5f670
Emily Sharp <emilysharp@Emilys-MacBook-Air.local>
2017-11-07 10:44:43 -0600

    px to rem / single page layout

--- server/controllers/contactform.ctrl.ts
+++ server/controllers/contactform.ctrl.ts
@@ -3,14 +3,14 @@
 
 // const router = Router();
 
-router.post('/', (req,res)=>{
-    sendEmail( 'stinson.lyon@gmail.com', req.body.from, req.body.subject, req.body.message, req.body.name)
-    .then((response: any)=>{
-        res.sendStatus(201);
-    }).catch((e:any)=>{
-        console.log(e);
-        res.sendStatus(500);
-    });
-});
+// router.post('/', (req,res)=>{
+//     sendEmail( 'stinson.lyon@gmail.com', req.body.from, req.body.subject, req.body.message, req.body.name)
+//     .then((response: any)=>{
+//         res.sendStatus(201);
+//     }).catch((e:any)=>{
+//         console.log(e);
+//         res.sendStatus(500);
+//     });
+// });
 
 // export default router;
