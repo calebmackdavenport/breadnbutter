059120fa4fe9b03eefceeef78d82f2fa22a06f45
Stinson Lyon <Stinzo@Stinson-Lyons-MacBook-Pro.local>
2017-11-07 10:52:24 -0600

    Merging this silly shit

--- server/controllers/contactform.ctrl.ts
+++ server/controllers/contactform.ctrl.ts
@@ -1,16 +1,16 @@
-import { Router } from 'express';
-import { sendEmail } from '../services/email.svc';
+// import { Router } from 'express';
+// // import { sendEmail } from '../services/email.svc';
 
-const router = Router();
+// const router = Router();
 
-router.post('/', (req,res)=>{
-    sendEmail( 'stinson.lyon@gmail.com', req.body.from, req.body.subject, req.body.message, req.body.name)
-    .then((response: any)=>{
-        res.sendStatus(201);
-    }).catch((e:any)=>{
-        console.log(e);
-        res.sendStatus(500);
-    });
-});
+// router.post('/', (req,res)=>{
+//     sendEmail( 'stinson.lyon@gmail.com', req.body.from, req.body.subject, req.body.message, req.body.name)
+//     .then((response: any)=>{
+//         res.sendStatus(201);
+//     }).catch((e:any)=>{
+//         console.log(e);
+//         res.sendStatus(500);
+//     });
+// });
 
-export default router;
+// export default router;
