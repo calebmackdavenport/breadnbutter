91f310a7d334e9a3ca009971493a9b8b252c4051
Stinson Lyon <Stinzo@Stinson-Lyons-MacBook-Pro.local>
2017-11-07 09:59:00 -0600

    Modified email.svc.ts to include multiple email recipients

--- server/services/email.svc.ts
+++ server/services/email.svc.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const sgMail = require('@sendgrid/mail');
 sgMail.setApiKey(process.env.SENDGRID_API_KEY || '');
 
@@ -18,24 +17,4 @@ export function sendEmail(to: string, from: string, subject: string, message: st
 
 
     return sgMail.send(mail);
-=======
-import * as sendgrid from 'sendgrid';
-const helper = sendgrid.mail;
-const sg = sendgrid(process.env.SENDGRID_API_KEY || '');
-
-export function sendEmail(to: string, from: string, subject: string, message: string) {
-    let toEmail = new helper.Email(to);
-    let fromEmail = new helper.Email(from);
-    let emailContent = new helper.Content('text/html', message);
-
-    let mail = new helper.Mail(fromEmail, subject, toEmail, emailContent);
-
-    let request = sg.emptyRequest({
-        method:'POST',
-        path:'v3/mail/send',
-        body: mail.toJSON()
-    });
-
-    return sg.API(request)
->>>>>>> 7d0316b7ff06e11d1720df7ec3036b7116bd6b0d
 }
