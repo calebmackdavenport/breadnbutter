a44c52e4666c9a53bf0bd9ed1a6317f5bb8b2e1a
Anil Joshi <aniljoshi@Anils-MacBook-Pro.local>
2017-11-08 15:37:49 -0600

    Fixed merge conflicts

--- server/procedures/userrecipes.proc.ts
+++ server/procedures/userrecipes.proc.ts
@@ -1,31 +1,9 @@
-<<<<<<< HEAD
-// TODO: get the procedures from Stinson
-=======
 import {row, rows, empty } from '../config/db';
->>>>>>> bcd74a947604148d425f2e40690cddea4b39624a
 
 export function getRecipes(): Promise<Array<models.IRecipe>> {
     return rows('GetAllRecipes');
 }
 
-<<<<<<< HEAD
-import { row, rows, empty } from '../config/db';
-
-export function all(): Promise<Array<models.IRecipe>> {
-    return rows(''); //GetAllUserRecipes
-}
-
-export function read(id: number): Promise<models.IRecipe> {
-    return row('', [id]); //GetSingleUserRecipe
-}
-
-// export function create(): Promise<models.ICreateResponse> {
-//     return row('', []); //AddUserRecipe
-// }
-
-export function destroy(id: number) {
-    return empty('', [id]); //DeleteUserRecipe
-=======
 export function getRecipe(id:number): Promise<models.IRecipe> {
     return row('GetSingleRecipe', [id]);
 }
@@ -36,7 +14,6 @@ export function getUserRecipes(userid:number): Promise<Array<models.IRecipe>> {
 
 export function addRecipe(userid:number, name:string, preptime:number, cooktime:number, servingsize:number, servingyield: string, ingredients: string, directions: string, additionalinfo: string){
     return rows('AddRecipe', [userid, name, preptime, cooktime, servingsize, servingyield, ingredients, directions, additionalinfo])
->>>>>>> bcd74a947604148d425f2e40690cddea4b39624a
 }
 
 export function deleteRecipe(id:number): Promise<void>{
