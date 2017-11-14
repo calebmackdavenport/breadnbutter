edf8c5c9560eab2f8de70559bcb64f55ee6eb889
Stinson Lyon <Stinzo@Stinson-Lyons-MacBook-Pro.local>
2017-11-14 16:04:50 -0600

    Delete button change

--- client/assets/deletebtn.js
+++ client/assets/deletebtn.js
@@ -1,15 +1,4 @@
 // Reset
-<<<<<<< HEAD
-$( document ).ready(function() {
-    $("button").on('mouseout', function(){
-        if($(this).hasClass("confirm") || $(this).hasClass("done")){
-            setTimeout(function(){
-                $("button").removeClass("confirm").removeClass("done");
-                $("span").text("Delete");
-            }, 3000);
-        }
-    })
-=======
 
 $("button").on('mouseout', function(){
     if($(this).hasClass("confirm") || $(this).hasClass("done")){
@@ -18,7 +7,6 @@ $("button").on('mouseout', function(){
             $("span").text("Delete");
         }, 3000);
     }
->>>>>>> a3adc4516ca0d8702f353bd3e530fc0b5c00e0dd
 });
 
 $('#myModal').on('shown.bs.modal', function () {
