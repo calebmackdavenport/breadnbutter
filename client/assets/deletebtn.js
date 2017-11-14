a214e3d7c709d6a8a0b1c47e095ac8960ab25629
Stinson Lyon <Stinzo@Stinson-Lyons-MacBook-Pro.local>
2017-11-14 13:14:54 -0600

    Changed syntax in deletebtn.js to remove redlines

--- client/assets/deletebtn.js
+++ client/assets/deletebtn.js
@@ -1,10 +1,11 @@
 // Reset
 $( document ).ready(function() {
-$("button").on('mouseout', function(){
-    if($(this).hasClass("confirm") || $(this).hasClass("done")){
-        setTimeout(function(){
-            $("button").removeClass("confirm").removeClass("done");
-            $("span").text("Delete");
-        }, 3000);
-    }
+    $("button").on('mouseout', function(){
+        if($(this).hasClass("confirm") || $(this).hasClass("done")){
+            setTimeout(function(){
+                $("button").removeClass("confirm").removeClass("done");
+                $("span").text("Delete");
+            }, 3000);
+        }
+    })
 });
